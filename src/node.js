91d3ddfe8b196bdae8cde947e0cf01f874956d21
IrinaAgeenko <ira.ageenkoo@gmail.com>
2017-10-07 00:47:24 +0300

    some methods in heap & queue@

--- src/node.js
+++ src/node.js
@@ -3,25 +3,31 @@ class Node {
 		this.data = data;
 		this.priority = priority;
 		this.parent = null;
-		this.next = null;
 		this.right = null;
 		this.left = null;
 	}
 
 	appendChild(node) {
-		if(this.left === null){
+		
+		this.parent = this;
+		 if(this.left === null){
 			this.left = node;
-		}
+			node.parent = this.parent;
+
+			}
 		else if(this.right === null){
-			this.right = node;
-		}
+				this.right = node;
+				node.parent = this.parent;
+			}
 	}
 
 	removeChild(node) {
 		if(node === this.left){
+			this.left.parent = null;
 			this.left = null;
 		}
 		else if(node === this.right){
+			this.right.parent = null;
 			this.right = null;
 		}
 		else{
@@ -30,14 +36,14 @@ class Node {
 	}
 
 	remove() {
-		if (this.parent !== null){
-			//removeChild();
-			//this.parent.removeChild(this.parent);
-		}
+		if(this.parent !== null)
+			this.parent.removeChild(this);
 	}
 
 	swapWithParent() {
-		
+		//console.log(this.parent.parent)
+		//this.parent.parent = this;
+
 	}
 }
 
--- src/queue.js
+++ src/queue.js
@@ -2,11 +2,19 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+		if (maxSize !== undefined){
+			this.maxSize = maxSize;
+		}
+		else{
+			this.maxSize = 30;
+		}
+		this.heap = new MaxHeap();
 	}
 
 	push(data, priority) {
-
+		if(data && priority){
+			this.heap.push(data, priority);
+		}
 	}
 
 	shift() {
